f02221b9a703190fc1a8b1d391158517aef24eee
Kyle Beasley <kyle.beasley17@gmail.com>
2025-06-06 13:09:27 -0500

    adjusted template path

--- src/Http/View.php
+++ src/Http/View.php
@@ -13,7 +13,7 @@ class View
     private TemplateWrapper $template;
 
     public function __construct(string $view_path) {
-        $this->fileSystemLoader = new FilesystemLoader('/templates');
+        $this->fileSystemLoader = new FilesystemLoader('templates');
         $this->twig = new Environment($this->fileSystemLoader);
         $template_path = str_replace('.', '/', strtolower($view_path));
         $this->template = $this->twig->load("$template_path.html.twig");
