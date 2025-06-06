c9be5476769255ec41ed3aa597a9b581db584d95
Kyle Beasley <kyle.beasley17@gmail.com>
2025-06-06 12:42:09 -0500

    getting this shit live with a router and some controllers

--- public/index.php
+++ public/index.php
@@ -1,24 +1,17 @@
 <?php
 
+$ROOT = dirname(__DIR__);
+require_once "$ROOT/vendor/autoload.php";
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
-require_once dirname(__DIR__) . '/vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable($ROOT);
+$dotenv->load();
 
-$var = "TEST VARIABLE";
+use Pecee\SimpleRouter\SimpleRouter;
+require_once "$ROOT/routes/web.php";
 
-?>
-
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>The GPE</title>
-</head>
-<body>
-    <img src="./img/Desk_Obliterated.webp" alt="Desk Obliterated" />
-
-    <h1><?php echo $var; ?></h1>
-</body>
-</html>
+SimpleRouter::setDefaultNamespace("App\Http\Controllers");
+SimpleRouter::start();
